7adf149f8f8640f7bd9fbbcba0f217f7b3bc2a4f
sarisv-L <sarisv@gmx.de>
2024-09-24 21:21:44 +0400

    Created Flexible Grid using Flexbox in a Container
    Grid can be adjusted by the users inputting an
    integer between 0 and 100. Container is fix.
    squares adjust to fit inside the container.

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const container = document.querySelector("#container");
+'use strict';
 
-for (let i = 0; i < 256; i++) {
-  const content = document.createElement("div");
-  content.classList.add("content");
-  container.appendChild(content);
+const container = document.getElementById('container');
+const newGridButton = document.getElementById('btn');
 
-  content.addEventListener("mouseover", () => {
-    content.style.backgroundColor = "yellow";
-  });
+function createGrid(squaresPerSide) {
+  //Clear existing Grid
+  container.innerHTML = '';
+
+  const totalSquares = squaresPerSide * squaresPerSide;
+  // prettier-ignore
+  const itemSize = (960 - (squaresPerSide * 2)) / squaresPerSide; // Calc size of each square
+
+  for (let i = 0; i < totalSquares; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.style.width = `${itemSize}px`;
+    content.style.height = `${itemSize}px`;
+    container.appendChild(content);
+  }
 }
+
+// Button Event Listener
+newGridButton.addEventListener('click', () => {
+  const userInput = prompt('Enter the number of squares per side(e.g., 16):');
+  const squaresPerSide = parseInt(userInput, 10);
+
+  if (
+    squaresPerSide > 0 &&
+    squaresPerSide < 100 &&
+    Number.isInteger(squaresPerSide)
+  ) {
+    createGrid(squaresPerSide);
+  } else {
+    alert('Please enter a valid positive Integer from 0-100.');
+  }
+});
+
+createGrid(16);
